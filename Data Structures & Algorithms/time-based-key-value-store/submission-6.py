f763ae00e19db0eb358d07c045d359def2aad3c7
Addiction <4827512+sylvestery@users.noreply.github.com>
2026-07-15 18:46:05 -0400

    Add: time-based-key-value-store - submission-6

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-6.py
@@ -0,0 +1,33 @@
+from sortedcontainers import SortedList
+class TimeMap:
+
+    def __init__(self):
+        self.store = defaultdict(list)
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.store[key].append((value, timestamp))
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.store:
+            return ""
+
+        times = self.store[key]
+        if timestamp < times[0][1]:
+            return ""
+        # Execute binary search to find the value closest to timestamp but not.
+        left = 0 
+        right = len(times) -1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if times[mid][1] == timestamp:
+                return times[mid][0]
+            if times[mid][1] < timestamp:
+                left = mid + 1
+            else:
+                right = mid -1
+        print(times, left, right)
+        return times[right][0] 
+
+        
